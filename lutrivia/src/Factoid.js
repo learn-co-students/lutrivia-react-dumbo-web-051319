1ed45d032d22cef249e030fc5ce20a473de68b0c
Rachel R <rrrachelrath@gmail.com>
2019-07-17 10:23:34 -0400

    Done.

--- lutrivia/src/Factoid.js
+++ lutrivia/src/Factoid.js
@@ -1,6 +1,21 @@
 import React from 'react';
 
 class Factoid extends React.Component{
+
+trueClick = () => {
+  if (this.props.answer === true){
+    console.log("it true")
+  }
+
+}
+
+falseClick = () => {
+  if (this.props.answer === false){
+    console.log("it false")
+  }
+
+}
+
   render(){
 
     // const trivia = this.props.trivia.map(quest => quest.text)
@@ -8,13 +23,15 @@ class Factoid extends React.Component{
     return (
       <div className="factoid">
       <p>{this.props.triviaText}</p>
-      <button>True</button>
-      <button>False</button>
+      <button className="true" onClick={this.trueClick} >True</button>
+      <button className="false" onClick={this.falseClick} >False</button>
       <p>{this.props.answer.toString()}</p>
       </div>
 
     )
   }
 }
+
+
 
 export default Factoid;
